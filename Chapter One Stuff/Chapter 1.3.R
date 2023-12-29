bf0922f8f6e1187dde1d9cf029ea24315c5eb529
luigiboy67 <shamgod98@pop-os.localdomain>
2023-12-29 08:39:56 -0600

    updates

--- Chapter One Stuff/Chapter 1.3.R
+++ Chapter One Stuff/Chapter 1.3.R
@@ -8,4 +8,52 @@
 # read_csv function also adds some nice user-oriented features like a progress meter and a compact method for specifying column types.
 
 library(readr)
-teams <- read_csv("STAT 330/beanie.csv")
+teams <- read_csv("data/team_standings.csv")
+
+cols(
+  Standing = col_double(),
+  Team = col_character()
+)
+
+teams
+# You can also specidy the type of each column with the col_types argument.
+
+teams <- read_csv("data/team_standings.csv", col_types = "cc")
+teams
+
+# The read_csv function will also read compressed files automatically.
+logs <- read_csv("data/2016-07-19.csv.gz", n_max = 10)
+
+── Column specification ────────────────────────────────────────────────────────
+cols(
+  date = col_date(format = ""),
+  time = col_time(format = ""),
+  size = col_double(),
+  r_version = col_character(),
+  r_arch = col_character(),
+  r_os = col_character(),
+  package = col_character(),
+  version = col_character(),
+  country = col_character(),
+  ip_id = col_double()
+)
+
+logs <- read_csv("data/2016-07-20.csv.gz", col_types = "ccicccccci", n_max = 10)
+logs
+
+# You can specify the column type in a more detailed fashion by using the various col_* functions.
+
+logdates <- read_csv("data/2016-07-20.csv.gz", 
+                     col_types = cols_only(date = col_date()),
+                     n_max = 10)
+logdates
+
+# read_csv progress option provides a progress meter while the csv file is being read.
+
+# readr function           Use
+# read_csv                 Reads comma-separated file
+# read_csv2                Reads semicolon-separated file
+# read_tsv                 Reads tab-separated file
+# read_delim               General function for reading delimited files
+# read_fwf                 Reads fixed width files
+# read_log                 Reads log files
