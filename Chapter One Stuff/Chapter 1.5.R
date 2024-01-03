804e1d3cc113b568f37c31a45038a0fddd23a5ad
luigiboy67 <shamgod98@pop-os.localdomain>
2024-01-03 09:15:24 -0600

    update file

--- Chapter One Stuff/Chapter 1.5.R
+++ Chapter One Stuff/Chapter 1.5.R
@@ -113,4 +113,129 @@ ext_tracks %>%
 head(ext_tracks$hour)
 head(ext_tracks$hour == "00")
 
-# 
+# when using logical statements with filter, it just returns whatever is true:
+ext_tracks %>% 
+  select(storm_name, hour, max_wind) %>%
+  head(9)
+
+ext_tracks %>%
+  select(storm_name, hour, max_wind) %>%
+  filter(hour == "00") %>%
+  head(3)
+
+# filter can also be done after summarizing:
+ext_tracks %>%
+  group_by(storm_name, year) %>%
+  summarize(worst_wind = max(max_wind)) %>%
+  filter(worst_wind >= 160)
+
+# you can also use the | and & operators as well
+ext_tracks %>%
+  select(storm_name, month, day, hour, latitude, longitude, max_wind) %>%
+  filter(storm_name == "ANDREW" & max_wind >= 137)
+
+# 1.5.4
+
+# mutate function can add, or change columns in the data frame.
+# lets use the worldcup dataset from faraway
+library(faraway)
+data(worldcup)
+
+# since the players names are row numbers, let's put this in its own category:
+worldcup <- worldcup %>%
+  mutate(player_name = rownames(worldcup))
+
+worldcup %>% slice(1:3)
+
+# you can use mutate in coordination with group_by to create new columns that gives summaries within certain windows of the data.
+worldcup <- worldcup %>% 
+  group_by(Position) %>%
+  mutate(ave_shots = mean(Shots)) %>%
+  ungroup()
+
+worldcup %>% slice(1:3)
+
+# use the rename function to rename a column
+worldcup %>% 
+  rename(Name = player_name) %>%
+  slice(1:3)
+
+# 1.5.5
+
+# we'll use VADeaths again for this section.
+data("VADeaths")
+head(VADeaths)
+
+# the gather function gather values spread across several columns into a single column, with the column names gathered into a "key" column.
+# use a minus sign to exclude columns
+data("VADeaths")
+library(tidyr)
+
+# Move age from row names into a column
+VADeaths  <- VADeaths %>%
+  tbl_df() %>%
+  mutate(age = row.names(VADeaths)) 
+VADeaths
+
+# Gather everything but age to tidy data
+VADeaths %>%
+  gather(key = key, value = death_rate, -age)
+
+# gather is occasionally useful for pulling data together to take advantage of faceting, or plotting separate plots based on a grouping variable.
+library(tidyr)
+library(ggplot2)
+worldcup %>%
+  select(Position, Time, Shots, Tackles, Saves) %>% 
+  gather(Type, Number, -Position, -Time) %>%
+  ggplot(aes(x = Time, y = Number)) + 
+  geom_point() + 
+  facet_grid(Type ~ Position)
+
+# spread function can be useful for creating summary tables.
+library(knitr)
+
+# Summarize the data to create the summary statistics you want
+wc_table <- worldcup %>% 
+  filter(Team %in% c("Spain", "Netherlands", "Uruguay", "Germany")) %>%
+  select(Team, Position, Passes) %>%
+  group_by(Team, Position) %>%
+  summarize(ave_passes = mean(Passes),
+            min_passes = min(Passes),
+            max_passes = max(Passes),
+            pass_summary = paste0(round(ave_passes), " (", 
+                                  min_passes, ", ",
+                                  max_passes, ")")) %>%
+  select(Team, Position, pass_summary)
+
+# What the data looks like before using `spread`
+wc_table
+
+# Use spread to create a prettier format for a table
+wc_table %>%
+  spread(Position, pass_summary) %>%
+  kable()
+# in the spread call, you first specify the name of the column to use for the new column names, then specify the column to use for the cell values
+# kable creates the summary table in a table format, rather than a basic R format.
+
+# 1.5.6
+
+# we'll use team_standings.csv file and the world standings dataset for joining data frames
+team_standings <- read_csv("data/team_standings.csv")
+team_standings %>% slice(1:3)
+
+# main arguments for the *_join functions are the object names of the two data frames to join and by, which specifies which variables to use to match up observations from the two dataframes.
+# Example
+world_cup <- worldcup
+left_join(world_cup, team_standings, by = "Team")
+
+# example of merging with dplyr functions 
+data(worldcup)
+worldcup %>% 
+  mutate(Name = rownames(worldcup),
+         Team = as.character(Team)) %>%
+  select(Name, Position, Shots, Team) %>%
+  arrange(desc(Shots)) %>%
+  slice(1:5) %>%
+  left_join(team_standings, by = "Team") %>% # Merge in team standings
+  rename("Team Standing" = Standing) %>%
+  kable()
