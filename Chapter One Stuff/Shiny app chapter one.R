5b6aea68673b44b6d01342c8e073797756d6b044
luigiboy67 <shamgod98@pop-os.localdomain>
2024-01-07 13:11:42 -0600

    add more menu items

--- Chapter One Stuff/Shiny app chapter one.R
+++ Chapter One Stuff/Shiny app chapter one.R
@@ -5,18 +5,18 @@ ui <- dashboardPage(
   dashboardHeader(
     title = "Chapter One"
   ),
-  dashboardSidebar(sidebarMenu(
-    id = "sidebar",
-    menuItem("1.1", tabName = "CrashCourse", icon = icon("dashboard")),
-    menuItem("1.2", tabName = "ImportanceOfTidyData", icon = icon("dashboard")),
-    menuItem("1.3", tabName = "RegualarTabularData", icon = icon("dashboard")),
-    menuItem("1.4", tabName = "Reading Web-Based Data", icon = icon("dashboard")),
-    menuItem("1.5", tabName = "BasicDataManipulation", icon = icon("dashboard")),
-    menuItem("1.6", tabName = "WorkingWithDatesTime", icon = icon("dashboard")),
-    menuItem("1.7", tabName = "TextProcessing",icon = icon("dashboard")),
-    menuItem("1.8", tabName = "TheRoleOfPhysicalMemory", icon = icon("dashboard")),
-    menuItem("1.9", tabName = "WorkingWithLargeDatasets", icon = icon("dashboard")),
-    menuItem("1.10", tabName = "DiagnosingProblemsf", icon = icon("dashboard")),
+  dashboardSidebar(
+                   sidebarMenu(id = "sidebar",
+    menuItem(text = "1.1", tabName = "CrashCourse", icon = icon("th")),
+    menuItem(text = "1.2", tabName = "ImportanceOfTidyData", icon = icon("th")),
+    menuItem(text = "1.3", tabName = "RegualarTabularData", icon = icon("dashboard")),
+    menuItem(text = "1.4", tabName = "Reading Web-Based Data", icon = icon("dashboard")),
+    menuItem(text = "1.5", tabName = "BasicDataManipulation", icon = icon("dashboard")),
+    menuItem(text = "1.6", tabName = "WorkingWithDatesTime", icon = icon("dashboard")),
+    menuItem(text = "1.7", tabName = "TextProcessing",icon = icon("dashboard")),
+    menuItem(text = "1.8", tabName = "TheRoleOfPhysicalMemory", icon = icon("dashboard")),
+    menuItem(text = "1.9", tabName = "WorkingWithLargeDatasets", icon = icon("dashboard")),
+    menuItem(text = "1.10", tabName = "DiagnosingProblemsf", icon = icon("dashboard"))
   )),
   dashboardBody()
 )
@@ -26,3 +26,4 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
