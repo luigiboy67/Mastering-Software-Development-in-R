eb8904875286700086de6f39afa5a36793e89aa9
ShaMarcus Anders <shamarcusanders@pop-os.localdomain>
2024-01-10 08:14:23 -0600

    Chapter 1.8 notes

---
+++ Chapter One Stuff/Chapter 1.8.R
@@ -0,0 +1,7 @@
+# 1.8
+
+# the learning objectives of this section are to:
+# describe how memory is used in r sessions to store objects
+
+# the pryr package provides a number of useful functions for interrogating the memory usage of your R sessions.
+# mme
