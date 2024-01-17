94e49ae2a3410f7bdb1c334466f11c08d5e2e228
luigiboy67 <shamgod98@gmail.com>
2024-01-17 12:37:38 -0600

    add Chapter 2 folder and 2.1 notes

---
+++ Chapter Two Stuff/Chapter 2.1.R
@@ -0,0 +1,12 @@
+# 2.1
+
+# The learning objectives of the section are:
+
+# Describe the control flow of an R program
+
+# Control structures in R allow you to control the flow of execution of a series of R expressions.
+
+# 2.1.1
+
+# if-else allows you to test a condition and act on it depending on whether it's true or false
+
