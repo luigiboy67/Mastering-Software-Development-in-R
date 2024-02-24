da6b19f1e11f1ba0f530c512eff739629839a091
luigiboy67 <shamgod98@gmail.com>
2024-02-24 11:45:49 -0600

    update and complete Chapte 2.3.R

--- Chapter Two Stuff/Chapter 2.3.R
+++ Chapter Two Stuff/Chapter 2.3.R
@@ -208,3 +208,102 @@ walk(c("Friends, Romans, countrymen,",
        "not to praise him."), message)
 
 # 2.3.3.3
+
+# recursion functions have two main parts: a few easy to solve problems called "base cases", and then a case for more complicated problems where the function is called inside of itself
+# function that adds all of the numbers in a vector:
+vector_sum_loop <- function(v){
+  result <- 0
+  for(i in v){
+    result <- result + i
+  }
+  result
+}
+
+vector_sum_loop(c(5, 40, 91))
+
+# to solve thisn recursively:
+vector_sum_rec <- function(v){
+  if(length(v) == 1){
+    v
+  } else {
+    v[1] + vector_sum_rec(v[-1])
+  }
+}
+
+vector_sum_rec(c(5, 40, 91))
+
+# Another exercise for thinking about applications for recursion is computing the Fibonacci sequence.
+# Here's a function
+fib <- function(n){
+  stopifnot(n > 0)
+  if(n == 1){
+    0
+  } else if(n == 2){
+    1
+  } else {
+    fib(n - 1) + fib(n - 2)
+  }
+}
+
+fib(1)
+fib(2)
+fib(3)
+fib(4)
+fib(5)
+fib(6)
+fib(7)
+
+purrr::map_dbl(1:12, fib)
+
+# memoization is a technique that stores the value of each calculated Fibonacci number in table so that once a number is calculated you can look it up instead of needing to recalculate it
+# Here's an example
+fib_tbl <- c(0, 1, rep(NA, 23))
+
+fib_mem <- function(n){
+  stopifnot(n > 0)
+  
+  if(!is.na(fib_tbl[n])){
+    fib_tbl[n]
+  } else {
+    fib_tbl[n - 1] <<- fib_mem(n - 1)
+    fib_tbl[n - 2] <<- fib_mem(n - 2)
+    fib_tbl[n - 1] + fib_tbl[n - 2]
+  }
+}
+
+purrr::map_dbl(1:12, fib_mem)
+
+# let's test to see if it's faster
+library(purrr)
+library(microbenchmark)
+library(tidyr)
+library(magrittr)
+library(dplyr)
+
+fib_data <- map(1:10, function(x){microbenchmark(fib(x), times = 100)$time})
+names(fib_data) <- paste0(letters[1:10], 1:10)
+fib_data <- as.data.frame(fib_data)
+
+fib_data %<>%
+  gather(num, time) %>%
+  group_by(num) %>%
+  summarise(med_time = median(time))
+
+memo_data <- map(1:10, function(x){microbenchmark(fib_mem(x))$time})
+names(memo_data) <- paste0(letters[1:10], 1:10)
+memo_data <- as.data.frame(memo_data)
+
+memo_data %<>%
+  gather(num, time) %>%
+  group_by(num) %>%
+  summarise(med_time = median(time))
+
+plot(1:10, fib_data$med_time, xlab = "Fibonacci Number", ylab = "Median Time (Nanoseconds)",
+     pch = 18, bty = "n", xaxt = "n", yaxt = "n")
+axis(1, at = 1:10)
+axis(2, at = seq(0, 350000, by = 50000))
+points(1:10 + .1, memo_data$med_time, col = "blue", pch = 18)
+legend(1, 300000, c("Not Memoized", "Memoized"), pch = 18, 
+       col = c("black", "blue"), bty = "n", cex = 1, y.intersp = 1.5)
+
+# from the graph, fib() grows exponentially, while fib_mem() stays constant 
